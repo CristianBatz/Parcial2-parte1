5b82f96aa7bff86f07a07a2d3535c278f981ea5e
diego192599 <di.cramire@udeo.edu.gt>
2025-09-08 10:14:31 -0600

    creacion de clase de candidatas

--- Concurso.py
+++ Concurso.py
@@ -1,2 +1,9 @@
-class Prueba:
-    pass
+class Candidatas:
+    def __init__(self ,codigo,nombre,edad,institucion,municipio):
+        self.codigo=codigo
+        self.nombre=nombre
+        self.edad=edad
+        self.institucion=institucion
+        self.municipio=municipio
+
+    def mostrar_info(self):
