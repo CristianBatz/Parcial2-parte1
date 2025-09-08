b751a8782e8cfdec3e40effcb6d373d284928723
CristianBatz <ce.sbatz@udeo.edu.gt>
2025-09-08 10:21:41 -0600

    se creo la clase calificaciones ademas del metodo promedio y se comenzo con la clase concurso

--- Concurso.py
+++ Concurso.py
@@ -1,2 +1,18 @@
-class Prueba:
-    pass
+class Calificacion:
+    def __init__(self,jurado,candidata, cultura,proyeccion,entrevista,promedio):
+        self.jurado = jurado
+        self.candidata = candidata
+        self.cultura = cultura
+        self.proyeccion = proyeccion
+        self.entrevista = entrevista
+        self.promedio = float(promedio)
+
+    def promedio(self):
+        promedio = (self.cultura + self.proyeccion + self.entrevista) / 3
+        return promedio
+
+
+class Concurso:
+    def __init__(self):
+        self.jurado = {}
+        self.candidata = {}
